0c7b7f66c1b7f469dabf9c4a558e8e06d96d0e28
root <root@rt.umw.local>
2016-07-07 10:51:02 -0400

    Add existing file

---
+++ tickets.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#RT Ticket Creator
+from rtkit.resource import RTResource
+from rtkit.authenticators import CookieAuthenticator
+from rtkit.errors import RTResourceError
+from rtkit import set_logging
+import logging
+set_logging('debug')
+logger = logging.getLogger('rtkit')
+import csv
+resource = RTResource('http://localhost/REST/1.0/', 'smelendy', 'ki11ercars', CookieAuthenticator)
+with open('tickets.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+	content = {
+        	'content': {
+                	'Queue': ('%s' % row[0]),
+                	'Subject': ('%s' % row[1]),
+                	'Text': ('%s' % row[2]),
+                	'Cc': ('%s' % row[3]),
+                	'AdminCc' : ('%s' % row[4]), 
+                        'Owner': ('%s' % row[5]),
+                	'Starts': ('%s' % row[6]),
+                	'Due':  ('%s' % row[7]), 
+        	}
+	}
+
+	try:
+		response = resource.post(path='ticket/new', payload=content,)
+		logger.info(response.parsed)
+	except RTResourceError as e:
+		logger.error(e.response.status_int)
+		logger.error(e.response.status)
+		logger.error(e.response.parsed)
+
+
+
---
+++ ticketsreport.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/rt4/bin/rt ls -o -Created -t ticket "Status != 'resolved'" -q 6 -f id,subject,created,id| grep "`date +"%a %b %d"`"   > /tmp/out
+sed 's/$/ "http:\/\/rt.umw.local\/Ticket\/Display.html=/' /tmp/out |awk {'print $0$1'}| awk {'print $1","$2,$3,$4 ","$NF'} > ticketsreport.csv
